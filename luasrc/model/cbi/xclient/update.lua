8c048e3c50adade2eb64dfd01f442c9221ab6df9
xcode75 <87862554+xcode75@users.noreply.github.com>
2022-01-27 08:41:23 +0000

    Update update.lua

--- luasrc/model/cbi/xclient/update.lua
+++ luasrc/model/cbi/xclient/update.lua
@@ -5,10 +5,6 @@ s = m:section(TypedSection, "xclient")
 s.anonymous = true
 s.addremove=false
 
-s:append(Template("xclient/xray_update"))
-
-s:append(Template("xclient/update_geoip"))
-
-s:append(Template("xclient/update_geosite"))
+s:append(Template("xclient/version"))
 
 return m
